e0ee34ccc670b6d2cde5a1301a959c665a1d97b8
Zidni Rif'an <zidni.rifan7@gmail.com>
2020-05-16 11:24:22 +0700

    edit main.js

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+// Load page content
+
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .querySelectorAll("#nav-desktop a, #nav-mobile a")
+    .forEach(function (elm) {
+      elm.addEventListener("click", function (event) {
+        // Muat konten halaman yang dipanggil
+        page = event.target.parentElement.getAttribute("href").substr(1);
+        loadPage(page);
+      });
+    });
+
+  var page = window.location.hash.substr(1);
+  if (page == "") page = "home";
+  loadPage(page);
+
+  function loadPage(page) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4) {
+        var content = document.querySelector("#body-content");
+        if (this.status == 200) {
+          content.innerHTML = xhttp.responseText;
+        } else if (this.status == 404) {
+          content.innerHTML = "<p>Halaman tidak ditemukan.</p>";
+        } else {
+          content.innerHTML = "<p>Ups.. halaman tidak dapat diakses.</p>";
+        }
+      }
+    };
+    xhttp.open("GET", "pages/" + page + ".html", true);
+    xhttp.send();
+  }
+});
